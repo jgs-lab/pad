ef0d4239428f380d046de8fefa3915e413857c40
Sean Bennett <sean.c.bennett7@gmail.com>
2015-03-19 10:49:48 -0700

    New Interface
    interface.php
        -Added the socket programming to read the socket from the blackbox
        -Still need to get the actual ip address for blackbox
    padValidation.html
        -Removed hardcoded videoData
        -created an alert for testing (will be replaced with way to start
    video and then run ajax calls)

--- interface.php
+++ interface.php
@@ -1,4 +1,16 @@
 <?php
-	$str = "This is just a test.";
-	echo json_encode($str);
+	//$str = "This is just a test.";
+	//echo json_encode($str);
+	//the host that is being connected to
+	$host = "localhost";
+	//the port being read
+	$port = 7474;
+
+	//set_time_limit(2); 
+	//create the socket
+	$sock = socket_create(AF_INET, SOCK_STREAM, 0);
+	socket_connect($sock, $host, $port);
+	$input = socket_read($sock, 1024);
+	echo $input;
+	socket_close($sock);
 ?>
