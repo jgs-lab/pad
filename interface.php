a4499dcd0320fe98e261088877e27c7e99e093c1
Sean Bennett <sean.c.bennett7@gmail.com>
2015-03-22 18:56:45 -0700

    Update to interface.php
    interface.php
        - Changed from socket_read to socket_recv
    padvalidation.html
        - Changed the placement of the ajax call to interface.

--- interface.php
+++ interface.php
@@ -1,16 +1,16 @@
 <?php
-	//$str = "This is just a test.";
-	//echo json_encode($str);
 	//the host that is being connected to
 	$host = "localhost";
 	//the port being read
 	$port = 7474;
 
-	//set_time_limit(2); 
 	//create the socket
-	$sock = socket_create(AF_INET, SOCK_STREAM, 0);
-	socket_connect($sock, $host, $port);
-	$input = socket_read($sock, 1024);
-	echo $input;
-	socket_close($sock);
+	$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	if(socket_connect($sock, $host, $port))
+	{
+		$input = "";
+		socket_recv($sock, $input, 64, MSG_WAITALL);
+		echo $input;
+		socket_close($sock);
+	}
 ?>
