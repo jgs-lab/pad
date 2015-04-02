0de698f16d5a0910839ad232c91bfdcfda948338
Sean Bennett <sean.c.bennett7@gmail.com>
2015-04-02 09:31:51 -0700

    CSV functionality
    interface.php
        -Changed where the socket was created and added an action from the
    URL allowing us to call different actions on the interface… either
    download or retrieve data
    padValidation.html
        -Changed the embedded video’s to iframes for stability
        -added a button and ajax call to download the file as a cvs

--- interface.php
+++ interface.php
@@ -3,18 +3,33 @@
 	$host = "localhost";
 	//the port being read
 	$port = 7474;
-
 	//create the socket
-	$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	if(socket_connect($sock, $host, $port))
+	$action = $_GET["action"];
+	if(!$action)
 	{
-		$input = "";
-		socket_recv($sock, $input, 33, MSG_WAITALL);
-		echo json_encode($input);
-		socket_close($sock);
-	}else{
+		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if(socket_connect($sock, $host, $port))
+		{
+			$input = "";
+			socket_recv($sock, $input, 33, MSG_WAITALL);
+			echo json_encode($input);
+			socket_close($sock);
+		}else{
 
-		echo json_encode("Unable to connect to socket");
-		socket_close($sock);
+			echo json_encode("Unable to connect to socket");
+			socket_close($sock);
+		}
+	}else
+	{
+		header('Content-Type: text/csv; charset=utf-8');
+		header('Content-Disposition: attachment; filename=data.csv');
+		$output = fopen('php://output', 'w');
+		$stuff = json_decode($action);
+		$i = array("video",0); //video counter
+		foreach ($stuff as $key => $value) {
+			fputcsv($output, $i); //adds the video title
+			fputcsv($output, $value); // adds the  data
+			$i[1]++; // increments to the next video.
+		}
 	}
 ?>
