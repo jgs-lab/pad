2aee087e519d9da2f7820dcf9c638c42fd8b130f
Sean Bennett <sean.c.bennett7@gmail.com>
2015-04-07 15:23:41 -0700

    Updated CSV Functionality
    interface.php
        -Changed From GET to POST to not limit the data size allowed to send
        -Changed error to echo to actual string socket error
    padValidation.html
        -Line 97 changed from GET to POST Data Size constraint
        -Changed the window open to a form with the video data as the value
    being sent (POST)
        -Cleared the data table between admin views

--- interface.php
+++ interface.php
@@ -4,7 +4,7 @@
 	//the port being read
 	$port = 7474;
 	//create the socket
-	$action = $_GET["action"];
+	$action = $_POST["action"];
 	if(!$action)
 	{
 		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -15,8 +15,7 @@
 			echo json_encode($input);
 			socket_close($sock);
 		}else{
-
-			echo json_encode("Unable to connect to socket");
+			echo json_encode(socket_strerror(socket_last_error($sock)));
 			socket_close($sock);
 		}
 	}else
@@ -25,11 +24,12 @@
 		header('Content-Disposition: attachment; filename=data.csv');
 		$output = fopen('php://output', 'w');
 		$stuff = json_decode($action);
-		$i = array("video",0); //video counter
+		$vidNum = 0;
 		foreach ($stuff as $key => $value) {
+			$i = array("video" . $vidNum); //video counter
 			fputcsv($output, $i); //adds the video title
 			fputcsv($output, $value); // adds the  data
-			$i[1]++; // increments to the next video.
+			$vidNum++; // increments to the next video.
 		}
 	}
 ?>
