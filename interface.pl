78090153cf58128c1b8425fd24f343cfd5e9a223
seanwars <sean.c.bennett7@gmail.com>
2015-11-23 12:37:06 -0700

    Update on the perl script
    Update on the perl script that lets us read for the socket for a
    specific amount of time.

--- interface.pl
+++ interface.pl
@@ -1,3 +1,4 @@
+use warnings;
 use strict;
 use Socket;
 
@@ -8,6 +9,11 @@ my $server = "localhost";  # Host IP running the server
 
 my $timestamp = localtime();
 
+$SIG{ALRM} = sub { 
+	print "done";
+	die "timeout"; 
+};
+
 # create the socket, connect to the port
 socket(SOCKET,PF_INET,SOCK_STREAM,(getprotobyname('tcp'))[2])
    or die "Can't create a socket $!\n";
@@ -15,9 +21,16 @@ connect( SOCKET, pack_sockaddr_in($port, inet_aton($server)))
    or die "$timestamp Can't connect to port $port! \n";
 
 my $line;
-while ($line = <SOCKET>) {
-		$timestamp = localtime();
-		print "$timestamp ";
-        print "$line\n";
-}
+eval {
+	alarm(3);
+	while (1) {
+			$line = <SOCKET>;
+			$timestamp = localtime();
+			print "$timestamp ";
+	        print "$line\n";
+	        sleep(.1);
+	}
+	alarm(0);
+};
+
 close SOCKET or die "close: $!";
